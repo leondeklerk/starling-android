6731d3af85f3542f991aa3ed2efd53278457a8a3
Leon de Klerk <developer@leondeklerk.com>
2021-05-13 01:59:31 +0200

    Document LibraryFragment.kt
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- app/src/main/java/com/leondeklerk/starling/library/LibraryFragment.kt
+++ app/src/main/java/com/leondeklerk/starling/library/LibraryFragment.kt
@@ -10,26 +10,31 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.leondeklerk.starling.databinding.FragmentLibraryBinding
 
+/**
+ * A simple [Fragment] responsible for showing all media on the device (device only) in a folder structure.
+ * This is also responsible for setting sync settings on all folders.
+ */
 class LibraryFragment : Fragment() {
 
     private lateinit var libraryViewModel: LibraryViewModel
     private var _binding: FragmentLibraryBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        // Instantiate the viewmodel
         libraryViewModel =
             ViewModelProvider(this).get(LibraryViewModel::class.java)
 
+        // Inflate the bindings
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Update the sample text.
         val textView: TextView = binding.textLibrary
         libraryViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
